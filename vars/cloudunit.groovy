66dd7408b6789801c0434d34a3e62b22335e9788
William Bartlett <bartlettstarman@gmail.com>
2016-10-20 14:08:33 +0200

    Update cloudunit.groovy

--- vars/cloudunit.groovy
+++ vars/cloudunit.groovy
@@ -1,10 +1,12 @@
-def call(String host, String script) {
+def call(String host, String username, String password, String script) {
     def tag = env.BUILD_TAG
     def jarfile = "/tmp/cloudunit-cli-${tag}.jar"
     sh """
         wget -q ${host}/resources/CloudUnitCLI.jar -O ${jarfile}
         java -jar ${jarfile} <<<EOF
+        connect --login ${username} --password ${password} --host ${host}
         ${script}
+        disconnect
         EOF
         rm ${jarfile}
     """
