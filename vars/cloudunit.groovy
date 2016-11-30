48ca8614b0978942bdfad083b8a19c9ef49a3f82
William Bartlett <bartlettstarman@gmail.com>
2016-11-30 14:02:11 +0100

    switch herestring to heredoc

--- vars/cloudunit.groovy
+++ vars/cloudunit.groovy
@@ -2,7 +2,7 @@ def call(String host, String username, String password, String script) {
     def jarfile = "CloudUnitCLI.jar"
     sh """
         wget -q ${host}/resources/CloudUnitCLI.jar -O ${jarfile}
-        java -jar ${jarfile} <<<EOF
+        java -jar ${jarfile} <<EOF
         connect --login ${username} --password ${password} --host ${host}
         ${script}
         disconnect
